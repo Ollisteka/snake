80c797f45ba71a4135ca5ba7e1de09b50f393256
Ollisteka <zhukova.o.m@yandex.ru>
2017-11-16 10:27:04 +0500

    поправила тесты в соответствии с новой реализацией

--- src/tests/GameFieldPanelTest.java
+++ src/tests/GameFieldPanelTest.java
@@ -1,61 +1,62 @@
 package tests;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import GUI.GameFieldPanel;
+import java.awt.Point;
+import logic.Config;
+import logic.Level;
 import org.junit.Test;
-import logic.*;
-
-import java.awt.*;
-
-import static org.junit.Assert.*;
 
 public class GameFieldPanelTest {
 
-    @Test
-    public void isDead(){
-        Config config = new Config(25, 25, 25, 1000);
-        GameFieldPanel gf = new GameFieldPanel(config, new Level(config, "test"));
-        //gf.setTimerStop();
-        Point[] arr = new Point[9];
-        arr[0] = new Point(0, 0);
-        arr[1] = new Point(1, 0);
-        arr[2] = new Point(2, 0);
-        arr[3] = new Point(2, 1);
-        arr[4] = new Point(2, 2);
-        arr[5] = new Point(1, 2);
-        arr[6] = new Point(0, 2);
-        arr[7] = new Point(0, 1);
-        arr[8] = new Point(0, 0);
-        gf.setSnakeLocations(arr);
-        assertTrue(gf.isSnakeDead());
-    }
+  @Test
+  public void isDead() {
+    Config config = new Config(25, 25, 25, 1000);
+    GameFieldPanel gf = new GameFieldPanel(config, new Level(config, "test"));
+    //gf.setTimerStop();
+    Point[] arr = new Point[9];
+    arr[0] = new Point(0, 0);
+    arr[1] = new Point(1, 0);
+    arr[2] = new Point(2, 0);
+    arr[3] = new Point(2, 1);
+    arr[4] = new Point(2, 2);
+    arr[5] = new Point(1, 2);
+    arr[6] = new Point(0, 2);
+    arr[7] = new Point(0, 1);
+    arr[8] = new Point(0, 0);
+    gf.setSnakeLocations(arr);
+    assertTrue(gf.isSnakeDead());
+  }
 
-    @Test
-    public void isNotDead() {
-        Config config = new Config(25, 25, 25, 1000);
-        GameFieldPanel field = new GameFieldPanel(config, new Level(config, "test"));
-        Point[] arr = new Point[9];
-        arr[0] = new Point(0, 0);
-        arr[1] = new Point(1, 0);
-        arr[2] = new Point(2, 0);
-        arr[3] = new Point(2, 1);
-        arr[4] = new Point(2, 2);
-        arr[5] = new Point(1, 2);
-        arr[6] = new Point(0, 2);
-        arr[7] = new Point(0, 1);
-        arr[8] = new Point(1, 1);
-        field.setSnakeLocations(arr);
-        assertFalse(field.isSnakeDead());
-    }
+  @Test
+  public void isNotDead() {
+    Config config = new Config(25, 25, 25, 1000);
+    GameFieldPanel field = new GameFieldPanel(config, new Level(config, "test"));
+    Point[] arr = new Point[9];
+    arr[0] = new Point(0, 0);
+    arr[1] = new Point(1, 0);
+    arr[2] = new Point(2, 0);
+    arr[3] = new Point(2, 1);
+    arr[4] = new Point(2, 2);
+    arr[5] = new Point(1, 2);
+    arr[6] = new Point(0, 2);
+    arr[7] = new Point(0, 1);
+    arr[8] = new Point(1, 1);
+    field.setSnakeLocations(arr);
+    assertFalse(field.isSnakeDead());
+  }
 
-    @Test
-    public void outOfTheBounds() {
-        Config config = new Config(25, 25, 25, 1000);
-        GameFieldPanel field = new GameFieldPanel(config, new Level(config, "test"));
-        Point[] arr = new Point[3];
-        arr[0] = new Point(-1, 0);
-        arr[1] = new Point(0, 0);
-        arr[2] = new Point(1, 0);
-        field.setSnakeLocations(arr);
-        assertTrue(field.isSnakeDead());
-    }
+  @Test
+  public void outOfTheBounds() {
+    Config config = new Config(25, 25, 25, 1000);
+    GameFieldPanel field = new GameFieldPanel(config, new Level(config, "test"));
+    Point[] arr = new Point[3];
+    arr[0] = new Point(-1, 0);
+    arr[1] = new Point(0, 0);
+    arr[2] = new Point(1, 0);
+    field.setSnakeLocations(arr);
+    assertTrue(field.isSnakeDead());
+  }
 }
--- src/tests/SnakeTest.java
+++ src/tests/SnakeTest.java
@@ -1,80 +1,87 @@
 package tests;
 
-import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
 import logic.Snake;
-import static junit.framework.Assert.*;
+import org.junit.Test;
 
 public class SnakeTest {
 
-    @Test
-    public void createSnake() {
-        Snake snake = new Snake();
-        assertEquals(2, snake.getLength());
-        assertEquals(true, snake.looksRight());
-    }
+  @Test
+  public void createSnake() {
+    Snake snake = new Snake();
+    assertEquals(1, snake.getLength());
+    assertEquals(false, snake.looksRight());
+    assertEquals(false, snake.looksLeft());
+    assertEquals(false, snake.looksUp());
+    assertEquals(false, snake.looksDown());
+  }
 
-    @Test
-    public void moveUpSnake() {
-        Snake snake = new Snake();
-        snake.moveUp();
-        assertTrue(snake.looksUp());
-        assertFalse(snake.looksDown());
-        assertFalse(snake.looksRight());
-        assertFalse(snake.looksLeft());
-    }
+  @Test
+  public void moveUpSnake() {
+    Snake snake = new Snake();
+    snake.moveUp();
+    assertTrue(snake.looksUp());
+    assertFalse(snake.looksDown());
+    assertFalse(snake.looksRight());
+    assertFalse(snake.looksLeft());
+  }
 
-    @Test
-    public void moveDownSnake() {
-        Snake snake = new Snake();
-        snake.moveDown();
-        assertTrue(snake.looksDown());
-        assertFalse(snake.looksUp());
-        assertFalse(snake.looksRight());
-        assertFalse(snake.looksLeft());
-    }
+  @Test
+  public void moveDownSnake() {
+    Snake snake = new Snake();
+    snake.moveDown();
+    assertTrue(snake.looksDown());
+    assertFalse(snake.looksUp());
+    assertFalse(snake.looksRight());
+    assertFalse(snake.looksLeft());
+  }
 
-    @Test
-    public void doNotMoveLeftSnake() {
-        Snake snake = new Snake();
-        snake.moveLeft();
-        assertTrue(snake.looksRight());
-        assertFalse(snake.looksDown());
-        assertFalse(snake.looksLeft());
-        assertFalse(snake.looksUp());
-    }
+  @Test
+  public void doNotMoveLeftSnake() {
+    Snake snake = new Snake();
+    snake.moveRight();
+    snake.moveLeft();
+    assertTrue(snake.looksRight());
+    assertFalse(snake.looksDown());
+    assertFalse(snake.looksLeft());
+    assertFalse(snake.looksUp());
+  }
 
-    @Test
-    public void moveRightSnake() {
-        Snake snake = new Snake();
-        snake.moveRight();
-        assertTrue(snake.looksRight());
-        assertFalse(snake.looksDown());
-        assertFalse(snake.looksUp());
-        assertFalse(snake.looksLeft());
-    }
+  @Test
+  public void moveRightSnake() {
+    Snake snake = new Snake();
+    snake.moveRight();
+    assertTrue(snake.looksRight());
+    assertFalse(snake.looksDown());
+    assertFalse(snake.looksUp());
+    assertFalse(snake.looksLeft());
+  }
 
-    @Test
-    public void moveLeftSnake() {
-        Snake snake = new Snake();
-        snake.moveUp();
-        snake.moveLeft();
-        assertTrue(snake.looksLeft());
-        assertFalse(snake.looksDown());
-        assertFalse(snake.looksUp());
-        assertFalse(snake.looksRight());
-    }
+  @Test
+  public void moveLeftSnake() {
+    Snake snake = new Snake();
+    snake.moveUp();
+    snake.moveLeft();
+    assertTrue(snake.looksLeft());
+    assertFalse(snake.looksDown());
+    assertFalse(snake.looksUp());
+    assertFalse(snake.looksRight());
+  }
 
-    @Test
-    public void checkAddLength(){
-        Snake snake = new Snake();
-        snake.addLength();
-        assertEquals(3, snake.getLength());
-    }
+  @Test
+  public void checkAddLength() {
+    Snake snake = new Snake();
+    snake.addLength();
+    assertEquals(2, snake.getLength());
+  }
 
-    @Test
-    public void checkEatFood(){
-        Snake snake = new Snake();
-        snake.eatFood();
-        assertEquals(3, snake.getLength());
-    }
+  @Test
+  public void checkEatFood() {
+    Snake snake = new Snake();
+    snake.eatFood();
+    assertEquals(2, snake.getLength());
+  }
 }
